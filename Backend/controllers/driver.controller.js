d3fa68dc3812906c6c5f2b72fd74740e3911af0d
Abhijeet kumar <abhijeetkv2@gmail.com>
2025-01-24 23:15:40 +0530

    Remove token validation check from logout functionality

--- Backend/controllers/driver.controller.js
+++ Backend/controllers/driver.controller.js
@@ -72,10 +72,6 @@ const getDriverProfile = async (req, res, next) => {
 const logoutDriver = async (req, res, next) => {
     const token = req.cookies?.token || req.headers.authorization?.split(' ')[1];
 
-    if (!token) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
-
     await blacklistTokenModel.create(token);
 
     res.clearCookie('token');
