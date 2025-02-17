3db208ea73101d7c1d6d94abfa190eee21448a2e
Abhijeet kumar <abhijeetkv2@gmail.com>
2025-02-17 23:25:11 +0530

    ...

--- Frontend/src/pages/DriverSignup.jsx
+++ Frontend/src/pages/DriverSignup.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const DriverSignup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [driverData, setDriverData] = useState({}); 
+  const [driverData, setDriverData] = useState({});
+   
   
   const submitHandler = (e) => {
     e.preventDefault();   
